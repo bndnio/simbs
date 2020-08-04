a0a49a9281aa72b24fa107572c8774b20989ab52
Brendon Earl <bearl@uvic.ca>
2020-08-03 18:49:52 -0700

    Add basic Nav & refactor primary color variable

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <nav></nav>
+export default function Nav(props) {
+  return (
+    <nav>
+      SIMBS
+      <a href="https://www.joinit.org/o/south-island-mountain-bike-society">
+        <span className="join-button">Become a member</span>
+      </a>
+    </nav>
+  )
+}
