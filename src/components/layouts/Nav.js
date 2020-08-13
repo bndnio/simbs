7b8022a50eb30df997aa59681fa3e1209b3bea3f
Brendon Earl <bearl@uvic.ca>
2020-08-12 23:04:00 -0700

    Add nav

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,12 +1,55 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-export default function Nav(props) {
+function NavItem({ navItem }) {
+  if (!navItem) return null
+
+  return (
+    <a className="nav-item" href={navItem.page}>
+      {navItem.page_name}
+    </a>
+  )
+}
+
+function Nav({ data }) {
+  const doc = data.prismic.allNavigations.edges.slice(0, 1).pop()
+
   return (
     <nav>
-      SIMBS
+      <img src={doc.node.logo.url} alt={doc.node.logo.alt} />
+      <div className="nav-items">
+        {doc.node.navto.map((navItem) => (
+          <NavItem navItem={navItem} />
+        ))}
+      </div>
       <a href="https://www.joinit.org/o/south-island-mountain-bike-society">
         <span className="join-button">Become a member</span>
       </a>
     </nav>
+  )
+}
+
+export default function (props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query NavQuery {
+          prismic {
+            allNavigations {
+              edges {
+                node {
+                  navto {
+                    page
+                    page_name
+                  }
+                  logo
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => <Nav data={data}></Nav>}
+    />
   )
 }
