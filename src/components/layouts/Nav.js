57ffbcc82f1f2b5553319e601cefa3b763759601
Brendon Earl <bearl@uvic.ca>
2020-08-16 22:54:02 -0700

    Add navbar for mobile

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -21,6 +21,18 @@ const debounce = (fn) => {
   }
 }
 
+function NavItems({ navItems }) {
+  if (!navItems) return null
+
+  return (
+    <>
+      {navItems.map((navItem) => (
+        <NavItem navItem={navItem} />
+      ))}
+    </>
+  )
+}
+
 function NavItem({ navItem }) {
   if (!navItem) return null
 
@@ -32,19 +44,46 @@ function NavItem({ navItem }) {
 }
 
 function Nav({ data }) {
+  const [mobileMenu, setMobileMenu] = useState(false)
   const [scroll, setScroll] = useState(0)
+  const [navRef, setNavRef] = useState(null)
 
+  // Hook to watch scroll position for styling
   useEffect(() => {
-    document.addEventListener(
-      "scroll",
-      debounce(() => {
-        const scrollCheck = window.scrollY < 10
-        if (scrollCheck !== scroll) {
-          setScroll(scrollCheck)
-        }
-      }),
-      { passive: true }
-    )
+    const checkScroll = () => {
+      // Look at scroll position
+      const scrollAtTop = window.scrollY < 10
+      if (scrollAtTop !== scroll) {
+        setScroll(!scrollAtTop)
+      }
+    }
+    // Call on initial render
+    checkScroll()
+    const debouncedCheckScroll = debounce(checkScroll)
+    // Bind the event listener
+    document.addEventListener("scroll", debouncedCheckScroll, {
+      passive: true,
+    })
+    // Unbind the event listener on clean up
+    return () => {
+      document.removeEventListener("click", debouncedCheckScroll)
+    }
+  }, [])
+
+  // Hook to watch click position for mobile menu
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (navRef && !navRef?.contains(event.target)) {
+        setMobileMenu(false)
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("click", handleClickOutside)
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("click", handleClickOutside)
+    }
   }, [])
 
   const doc = data.prismic.allNavigations.edges.slice(0, 1).pop()
@@ -52,17 +91,43 @@ function Nav({ data }) {
   return (
     <nav>
       <div className="nav-placeholder"></div>
-      <div className={`nav-shadow ${scroll ? "" : "raised"}`}></div>
-      <div className={`nav-bar ${scroll ? "" : "raised"}`}>
-        <img src={doc.node.logo.url} alt={doc.node.logo.alt} />
-        <div className="nav-items">
-          {doc.node.navto.map((navItem) => (
-            <NavItem navItem={navItem} />
-          ))}
+      <div className={`nav-shadow ${scroll ? "raised" : ""}`}></div>
+      <div
+        ref={setNavRef}
+        className={`nav-bar ${scroll || mobileMenu ? "raised" : ""} ${
+          mobileMenu ? "force-shadow" : ""
+        }`}
+      >
+        <div className="nav-mobile">
+          {mobileMenu ? (
+            <span
+              className="material-icons"
+              onClick={() => setMobileMenu(false)}
+            >
+              close
+            </span>
+          ) : (
+            <span
+              className="material-icons"
+              onClick={() => setMobileMenu(true)}
+            >
+              menu
+            </span>
+          )}
         </div>
-        <a href="https://www.joinit.org/o/south-island-mountain-bike-society">
-          <span className="join-button">Become a member</span>
+        <img src={doc.node.logo.url} alt={doc.node.logo.alt} />
+        <div className="nav-desktop">
+          <NavItems navItems={doc.node.navto} />
+        </div>
+        <a
+          className="cta"
+          href="https://www.joinit.org/o/south-island-mountain-bike-society"
+        >
+          <span className={`join-button ${scroll ? "flat" : ""}`}>Join</span>
         </a>
+        <div className={`nav-mobile-drawer ${mobileMenu ? "visible" : ""}`}>
+          <NavItems navItems={doc.node.navto} />
+        </div>
       </div>
     </nav>
   )
