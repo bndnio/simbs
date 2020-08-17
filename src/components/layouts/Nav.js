68ea6dfb997ede833662c3a8fe1971d1ba3f5337
Brendon Earl <bearl@uvic.ca>
2020-08-16 19:08:14 -0700

    Allow nav to float

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,5 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { StaticQuery, graphql } from "gatsby"
+
+// The debounce function receives our function as a parameter
+const debounce = (fn) => {
+  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+  let frame
+
+  // The debounce function returns a new function that can receive a variable number of arguments
+  return (...params) => {
+    // If the frame variable has been defined, clear it now, and queue for next frame
+    if (frame) {
+      cancelAnimationFrame(frame)
+    }
+
+    // Queue our function call for the next frame
+    frame = requestAnimationFrame(() => {
+      // Call our function and pass any params we received
+      fn(...params)
+    })
+  }
+}
 
 function NavItem({ navItem }) {
   if (!navItem) return null
@@ -12,19 +32,38 @@ function NavItem({ navItem }) {
 }
 
 function Nav({ data }) {
+  const [scroll, setScroll] = useState(0)
+
+  useEffect(() => {
+    document.addEventListener(
+      "scroll",
+      debounce(() => {
+        const scrollCheck = window.scrollY < 10
+        if (scrollCheck !== scroll) {
+          setScroll(scrollCheck)
+        }
+      }),
+      { passive: true }
+    )
+  }, [])
+
   const doc = data.prismic.allNavigations.edges.slice(0, 1).pop()
 
   return (
     <nav>
-      <img src={doc.node.logo.url} alt={doc.node.logo.alt} />
-      <div className="nav-items">
-        {doc.node.navto.map((navItem) => (
-          <NavItem navItem={navItem} />
-        ))}
+      <div className="nav-placeholder"></div>
+      <div className={`nav-shadow ${scroll ? "" : "raised"}`}></div>
+      <div className={`nav-bar ${scroll ? "" : "raised"}`}>
+        <img src={doc.node.logo.url} alt={doc.node.logo.alt} />
+        <div className="nav-items">
+          {doc.node.navto.map((navItem) => (
+            <NavItem navItem={navItem} />
+          ))}
+        </div>
+        <a href="https://www.joinit.org/o/south-island-mountain-bike-society">
+          <span className="join-button">Become a member</span>
+        </a>
       </div>
-      <a href="https://www.joinit.org/o/south-island-mountain-bike-society">
-        <span className="join-button">Become a member</span>
-      </a>
     </nav>
   )
 }
