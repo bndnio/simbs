419077d6424023d345931886f219258ec2ec1d35
Brendon Earl <bearl@uvic.ca>
2020-12-13 21:56:41 -0800

    Add events page

---
+++ src/pages/events.js
@@ -0,0 +1,83 @@
+import React from "react"
+import { RichText } from "prismic-reactjs"
+import { graphql } from "gatsby"
+import { linkResolver } from "../utils/linkResolver"
+import htmlSerializer from "../utils/htmlSerializer"
+import Banner from "../components/Banner"
+import Layout from "../components/layouts"
+import Slices from "../components/slices"
+
+// Query for the Blog Home content in Prismic
+export const query = graphql`
+  {
+    prismic {
+      allEvents_pages {
+        edges {
+          node {
+            _meta {
+              id
+            }
+            title
+            subtitle
+            image
+            description
+            body {
+              ... on PRISMIC_Trails_pageBodyText {
+                type
+                label
+                primary {
+                  anchor
+                  text
+                  title
+                }
+              }
+              ... on PRISMIC_Trails_pageBodyText_with_embed {
+                type
+                label
+                primary {
+                  anchor
+                  text
+                  title
+                  raw_embed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+// Using the queried Events Page document data, we render the top section
+const TrailsHead = ({ page }) => {
+  return (
+    <div className="events-header" data-wio-id={page._meta.id}>
+      <Banner
+        url={page.image?.url}
+        title={page.title && RichText.asText(page.title)}
+        subtitle={page.subtitle && RichText.asText(page.subtitle)}
+      />
+
+      {page.description && (
+        <div className="container">
+          {RichText.render(page.description, linkResolver, htmlSerializer)}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ({ data }) => {
+  // Define the Blog Home & Blog Post content returned from Prismic
+  const doc = data.prismic.allTrails_pages.edges.slice(0, 1).pop()
+
+  if (!doc) return null
+
+  return (
+    <Layout>
+      <TrailsHead page={doc.node} />
+      <Slices slices={doc.node.body} />
+    </Layout>
+  )
+}
