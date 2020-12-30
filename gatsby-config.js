f25f4aae9940616aa5b86ca309e9ad5262a420dd
Brendon Earl <bearl@uvic.ca>
2020-12-29 21:10:19 -0800

    add gatsby-plugin-react-svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /src\/images/,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
