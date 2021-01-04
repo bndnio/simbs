4f81a42d4d889f9d19c4a0f0845cf52a6e4e91ee
Brendon Earl <bearl@uvic.ca>
2021-01-03 21:32:51 -0800

    Modify html serializer to use commonJS (and simplify)

--- src/utils/htmlSerializer.js
+++ src/utils/htmlSerializer.js
@@ -1,76 +1,116 @@
 // -- The HTML Serializer
 // This function will be used to modify the way that a Rich Text or Title field is rendered.
 
-import React from "react"
-import { Link as PrismicLink } from "prismic-reactjs"
-import { Elements } from "prismic-richtext"
-import { linkResolver } from "./linkResolver"
-import { Link } from "gatsby"
+var PrismicDOM = require("prismic-dom")
+var Elements = PrismicDOM.RichText.Elements
 
-export default function (type, element, content, children, index) {
-  // Generate links to Prismic Documents as <Link> components
-  if (type === Elements.hyperlink) {
-    let result = ""
-    const url = PrismicLink.url(element.data, linkResolver)
-
-    if (element.data.link_type === "Document") {
-      result = (
-        <Link to={url} key={index}>
-          {content}
-        </Link>
+module.exports = function htmlSerializer(type, element, content, children) {
+  switch (type) {
+    // Don't wrap images in a <p> tag
+    case Elements.image:
+      return (
+        '<img src="' +
+        element.url +
+        '" alt="' +
+        element.alt +
+        '" copyright="' +
+        element.copyright +
+        '">'
       )
-    } else {
-      const target = element.data.target
-        ? { target: element.data.target, rel: "noopener" }
-        : {}
-      result = (
-        <a className="link" href={url} {...target} key={index}>
-          {content}
-        </a>
+
+    // Add a class to hyperlinks
+    case Elements.hyperlink:
+      var target = element.data.target
+        ? 'target="' + element.data.target + '" rel="noopener"'
+        : ""
+      var linkUrl = PrismicDOM.Link.url(element.data, linkResolver)
+      return (
+        '<a class="link"' +
+        target +
+        ' href="' +
+        linkUrl +
+        '">' +
+        content +
+        "</a>"
       )
-    }
-    return result
+
+    // Return null to stick with the default behavior for all other elements
+    default:
+      return null
   }
-
-  // If the image is also a link to a Prismic Document, it will return a <Link> component
-  if (type === Elements.image) {
-    let result = (
-      <img
-        src={element.url}
-        alt={element.alt || ""}
-        copyright={element.copyright || ""}
-      />
-    )
-
-    if (element.linkTo) {
-      const url = PrismicLink.url(element.linkTo, linkResolver)
-
-      if (element.linkTo.link_type === "Document") {
-        result = (
-          <Link to={url} key={index}>
-            {result}
-          </Link>
-        )
-      } else {
-        const target = element.linkTo.target
-          ? { target: element.linkTo.target, rel: "noopener" }
-          : {}
-        result = (
-          <a className="link" href={url} {...target}>
-            {result}
-          </a>
-        )
-      }
-    }
-    const wrapperClassList = [element.label || "", "block-img"]
-    result = (
-      <p className={wrapperClassList.join(" ")} key={index}>
-        {result}
-      </p>
-    )
-    return result
-  }
-
-  // Return null to stick with the default behavior for everything else
-  return null
 }
+
+//  ---------- OLD Serializer ----------
+// import React from "react"
+// import { Link as PrismicLink } from "prismic-reactjs"
+// import { Elements } from "prismic-richtext"
+// import { linkResolver } from "./linkResolver"
+// import { Link } from "gatsby"
+
+// export default function (type, element, content, children, index) {
+//   // Generate links to Prismic Documents as <Link> components
+//   if (type === Elements.hyperlink) {
+//     let result = ""
+//     const url = PrismicLink.url(element.data, linkResolver)
+
+//     if (element.data.link_type === "Document") {
+//       result = (
+//         <Link to={url} key={index}>
+//           {content}
+//         </Link>
+//       )
+//     } else {
+//       const target = element.data.target
+//         ? { target: element.data.target, rel: "noopener" }
+//         : {}
+//       result = (
+//         <a className="link" href={url} {...target} key={index}>
+//           {content}
+//         </a>
+//       )
+//     }
+//     return result
+//   }
+
+//   // If the image is also a link to a Prismic Document, it will return a <Link> component
+//   if (type === Elements.image) {
+//     let result = (
+//       <img
+//         src={element.url}
+//         alt={element.alt || ""}
+//         copyright={element.copyright || ""}
+//       />
+//     )
+
+//     if (element.linkTo) {
+//       const url = PrismicLink.url(element.linkTo, linkResolver)
+
+//       if (element.linkTo.link_type === "Document") {
+//         result = (
+//           <Link to={url} key={index}>
+//             {result}
+//           </Link>
+//         )
+//       } else {
+//         const target = element.linkTo.target
+//           ? { target: element.linkTo.target, rel: "noopener" }
+//           : {}
+//         result = (
+//           <a className="link" href={url} {...target}>
+//             {result}
+//           </a>
+//         )
+//       }
+//     }
+//     const wrapperClassList = [element.label || "", "block-img"]
+//     result = (
+//       <p className={wrapperClassList.join(" ")} key={index}>
+//         {result}
+//       </p>
+//     )
+//     return result
+//   }
+
+//   // Return null to stick with the default behavior for everything else
+//   return null
+// }
