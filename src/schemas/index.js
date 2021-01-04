d852838eca490c870bbcea0f44c208f3d82c86a1
Brendon Earl <bearl@uvic.ca>
2021-01-03 18:35:17 -0800

    Add prismic custom type schemas

---
+++ src/schemas/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  about_page: require("./about_page.json"),
+  advocacy_page: require("./advocacy_page.json"),
+  author: require("./author.json"),
+  blog_home: require("./blog_home.json"),
+  blog_post_category: require("./blog_post_category.json"),
+  events_page: require("./events_page.json"),
+  home_page: require("./home_page.json"),
+  membership_page: require("./membership_page.json"),
+  navigation: require("./navigation.json"),
+  post: require("./post.json"),
+  sponsors: require("./sponsors.json"),
+  trails_page: require("./trails_page.json"),
+}
