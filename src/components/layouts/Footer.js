6e95e111360dc980d3dd5008b87e47278c607f8b
Brendon Earl <bearl@uvic.ca>
2021-01-05 20:23:29 -0800

    Remove simbs-logo-landscape from project

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,5 +1,4 @@
 import React from "react"
-import prismicLogo from "../../images/logo-prismic.svg"
 
 export default () => (
   <footer>
@@ -12,14 +11,6 @@ export default () => (
           rel="noopener noreferrer"
         >
           Brendon
-        </a>
-        <br />
-        <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-          <img
-            className="footer-logo"
-            src={prismicLogo}
-            alt="Gray Prismic logo"
-          />
         </a>
       </p>
     </div>
